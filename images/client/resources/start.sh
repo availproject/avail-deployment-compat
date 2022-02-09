ed243a3faf5e429335bec76d567b170cb328f617
miguelDD1 <miguel@polygon.technology>
2022-02-09 08:08:57 +0100

    Use YAML format on config

--- images/client/resources/start.sh
+++ images/client/resources/start.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 # Replace configration file 
-echo "Template"
+echo "## File config-template.yaml "
+echo "##############################"
 cat < ./config-template.yaml
 envsubst < ./config-template.yaml > ./config.yml
-echo "Generated config"
+echo ""
+
+echo "## Generated config.yaml"
+echo "##############################"
 cat < ./config.yml
+echo ""
 
 # Run the light-client
+echo "## Run light-client"
 ./avail-light
