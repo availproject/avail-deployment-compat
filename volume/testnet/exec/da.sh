88c27e9aae53421474b06f0a74ba2afa1879ceb3
John Hilliard <praetoriansentry@gmail.com>
2022-05-31 12:26:15 -0400

    Adding Empty Directories
    Adding some empty state and keystore directories for the purposes of
    tracking state in the local environment

--- volume/testnet/exec/da.sh
+++ volume/testnet/exec/da.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat /entrypoint.sh;
+cat /da/exec/da.sh
 
 trap cleanup 1 2 3 6
 
@@ -9,4 +9,17 @@ cleanup()
   exit 1
 }
 
+
+/da/bin/data-avail key insert --chain=/da/genesis/testnet-v2-local.chain.spec.raw.json --base-path=/da/state/ --keystore-path=/da/keystore/ --key-type=babe --scheme=Sr25519 --suri=/da/suri.txt
+/da/bin/data-avail key insert --chain=/da/genesis/testnet-v2-local.chain.spec.raw.json --base-path=/da/state/ --keystore-path=/da/keystore/ --key-type=gran --scheme=Ed25519 --suri=/da/suri.txt
+/da/bin/data-avail key insert --chain=/da/genesis/testnet-v2-local.chain.spec.raw.json --base-path=/da/state/ --keystore-path=/da/keystore/ --key-type=imon --scheme=Sr25519 --suri=/da/suri.txt
+/da/bin/data-avail key insert --chain=/da/genesis/testnet-v2-local.chain.spec.raw.json --base-path=/da/state/ --keystore-path=/da/keystore/ --key-type=audi --scheme=Sr25519 --suri=/da/suri.txt
+/da/bin/data-avail key insert --chain=/da/genesis/testnet-v2-local.chain.spec.raw.json --base-path=/da/state/ --keystore-path=/da/keystore/ --key-type=auth --scheme=Sr25519 --suri=/da/suri.txt
+
+echo "$@"
+
 /da/bin/data-avail $@
+
+
+
+
