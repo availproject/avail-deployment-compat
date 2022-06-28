f292e31d3fbc79fe66cc0995e939a61154394d19
John Hilliard <praetoriansentry@gmail.com>
2022-06-28 13:53:24 -0400

    adding outputs

--- tf/main.tf
+++ tf/main.tf
@@ -35,22 +35,3 @@ resource "aws_key_pair" "devnet" {
   public_key = var.devnet_key_value
 }
 
-
-
-
-
-
-output "ec2_full_node_ips" {
-  value = ["${aws_instance.full_node.*.private_ip}"]
-}
-output "ec2_validator_ips" {
-  value = ["${aws_instance.validator.*.private_ip}"]
-}
-output "ec2_explorer_ips" {
-  value = ["${aws_instance.explorer.*.private_ip}"]
-}
-
-output "alb_domain_name" {
-  value = aws_lb.avail_nodes.dns_name
-}
-
