6499867b922d731c03fb7258277cbae8408a1228
dan moore <dmoore@dans-MBP.attlocal.net>
2022-08-01 14:11:35 -0500

    full flow again

--- tf/modules/devnet/outputs.tf
+++ tf/modules/devnet/outputs.tf
@@ -1,21 +1,3 @@
-
-output "ec2_full_node_ips" {
-  value = aws_instance.full_node.*.private_ip
-}
-output "ec2_validator_ips" {
-  value = aws_instance.validator.*.private_ip
-}
-output "ec2_explorer_ips" {
-  value = aws_instance.explorer.*.private_ip
-}
-output "ec2_light_client_ips" {
-  value = aws_instance.light_client.*.private_ip
-}
-
-output "alb_domain_name" {
-  value = aws_lb.avail_nodes.dns_name
-}
-
 output "pk_ansible" {
   value     = tls_private_key.pk.private_key_pem
   sensitive = true
--- tf/nets/devnet01/outputs.tf
+++ tf/nets/devnet01/outputs.tf
@@ -1,19 +1,3 @@
-output "full_node_ips" {
-  value = module.{module_name}.ec2_full_node_ips
-}
-
-output "validator_ips" {
-  value = module.{module_name}.ec2_validator_ips
-}
-
-output "explorer_ips" {
-  value = module.{module_name}.ec2_explorer_ips
-}
-
-output "light_client_ips" {
-  value = module.{module_name}.ec2_light_client_ips
-}
-
 output "pk_ansible" {
   value     = module.{module_name}.pk_ansible
   sensitive = true
