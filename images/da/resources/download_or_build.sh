082341f67df6464d9f83c282760552b49ef8be9f
Luka Borkovic <luka.borkovic@ethernal.tech>
2023-10-17 19:01:44 +0200

    Add support for downloading binaries (if available) instead of building.

---
+++ images/da/resources/download_or_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if the binary version is provided as an argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <binary_version> [features]"
+    exit 1
+fi
+
+
+binary_version="$1"
+binary_url="https://github.com/availproject/avail/releases/download/$binary_version/data-avail-linux-amd64.tar.gz"
+
+
+binary_file="release.tar.gz"
+
+# Try to fetch the binary with wget
+wget -O "$binary_file" "$binary_url"
+
+# Check the exit status of wget
+if [ $? -eq 0 ]; then
+    echo "Binary ($binary_version) successfully fetched."
+    mkdir -p target/release
+    tar -xzf "$binary_file" -C target/release
+    mv target/release/data-avail-linux-amd64 target/release/data-avail
+else
+    echo "Building from source!"
+    cargo build --release -p da-runtime --features "$2"
+fi
---
+++ images/da/resources/download_or_build_runtime.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the binary version is provided as an argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <binary_version> [features]"
+    exit 1
+fi
+
+
+binary_version="$1"
+binary_url="https://github.com/availproject/avail/releases/download/$binary_version/data-avail-runtime.compact.compressed.wasm"
+
+
+binary_file="runtime.wasm"
+
+# Try to fetch the binary with wget
+wget -O "$binary_file" "$binary_url"
+
+# Check the exit status of wget
+if [ $? -eq 0 ]; then
+    echo "Binary ($binary_version) successfully fetched."
+    mkdir -p target/release/wbuild/da-runtime
+    mv runtime.wasm target/release/wbuild/da-runtime/da_runtime.compact.compressed.wasm 
+else
+    echo "Building from source!"
+    cargo build --release -p data-avail --features "$2"
+fi
