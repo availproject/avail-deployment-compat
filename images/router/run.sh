fa9b8bab6963fa820a8f4633fb3ac0462ff79f68
Vikram Bhattacharjee <53374818+vbhattaccmu@users.noreply.github.com>
2024-04-11 01:01:25 +0530

    chore: simplify egress traffic delay rule on router

--- images/router/run.sh
+++ images/router/run.sh
@@ -15,13 +15,6 @@ nft add table ip nat
 nft add chain ip nat postrouting { type nat hook postrouting priority 100 \; }
 nft add rule ip nat postrouting ip saddr $SUBNET_INTERNAL oifname "eth0" snat $ADDR_EXTERNAL
 
-# tc can only apply delays on egress traffic. By setting a delay for both eth0 and eth1, we achieve the active delay passed in as a parameter.
-half_of_delay=$(expr "$DELAY_MS" / 2 )
-param="${half_of_delay}ms"
-
-tc qdisc add dev eth0 root netem delay $param
-tc qdisc add dev eth1 root netem delay $param
-
 echo "1" > /tmp/setup_done # This will be checked by our docker HEALTHCHECK
 
 tail -f /dev/null # Keep it running forever.
