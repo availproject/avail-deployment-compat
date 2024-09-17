529bf4a851ae4f7aba0ceb53dffdcef4248eba33
rac-sri <rac.sri25@gmail.com>
2024-09-17 22:45:22 +0530

    feat: grs support

---
+++ images/grs/resources/download_or_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the binary version is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <binary_version>"
+    exit 1
+fi
+
+binary_version="$1"
+binary_url="https://github.com/availproject/avail-light/releases/download/$binary_version/avail-light-linux-amd64.tar.gz"
+
+
+binary_file="release.tar.gz"
+
+# Try to fetch the binary with wget
+wget -O "$binary_file" "$binary_url"
+
+# Check the exit status of wget
+if [ $? -eq 0 ]; then
+    echo "Binary ($binary_version) successfully fetched."
+    mkdir -p target/release
+    tar -xzf "$binary_file" -C target/release
+    mv target/release/avail-light-linux-amd64 target/release/avail-light-client
+else
+    echo "Building from source!"
+    cargo build --release
+fi
